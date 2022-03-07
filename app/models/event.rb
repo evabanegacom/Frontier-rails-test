0209a680dfbbc9df7aecb30b7f5c146718f4a4bd
Precious chiedu <udegbue69@gmail.com>
2022-03-07 20:35:35 +0100

    validated model params

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   has_many :player_events, dependent: :destroy
   has_many :attendees, class_name: 'Player', inverse_of: 'attended_events', through: :player_events,
   foreign_key: :event_id
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :course_name, presence: true, length: { minimum: 3 }
+  validates :date, presence: true, length: { minimum: 3 }
 
   def self.search(search)
     where('date LIKE ?', "%#{search}")
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,7 @@ class Player < ApplicationRecord
   has_many :player_events, dependent: :destroy
   has_many :attended_events, class_name: 'Event', inverse_of: 'attendees', through: :player_events,
   foreign_key: :player_id
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :handicap, presence: true, length: { minimum: 3 }
+  validates :location, presence: true, length: { minimum: 3 }
 end
